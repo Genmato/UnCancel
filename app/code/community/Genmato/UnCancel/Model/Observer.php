f2142b50f92c27e13db8d51a4d71bc89a6072dcd
Vladimir Kerkhoff <v.kerkhoff@genmato.com>
2014-06-03 06:16:32 +0200

    Release 1.0.1 - Fixed 404 error when clicking the uncancel button

--- app/code/community/Genmato/UnCancel/Model/Observer.php
+++ app/code/community/Genmato/UnCancel/Model/Observer.php
@@ -5,17 +5,20 @@ class Genmato_UnCancel_Model_Observer
 
     public function addButton($observer)
     {
-
         if ($observer->getEvent()->getBlock() instanceof Mage_Adminhtml_Block_Widget_Container &&
             $observer->getEvent()->getBlock()->getId() == 'sales_order_view' &&
             $this->getOrder()->getState() == 'canceled'
         ) {
 
             $observer->getEvent()->getBlock()
-                ->addButton('UnCancel', array(
-                    'label' => Mage::helper('adminhtml')->__('UnCancel'),
-                    'onclick' => 'setLocation(\'' . $this->getUncancelUrl() . '\')',
-                ), -1);
+                ->addButton(
+                    'UnCancel',
+                    array(
+                        'label' => Mage::helper('adminhtml')->__('UnCancel'),
+                        'onclick' => 'setLocation(\'' . $this->getUncancelUrl() . '\')',
+                    ),
+                    -1
+                );
 
         }
     }
